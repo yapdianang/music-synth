216cf79cc89afa4193136fe39f6d76fcc7a3072c
Avoy Datta <avoy.datta@stanford.edu>
2019-03-09 01:29:59 -0800

    Edited echo.v

--- final/echo.v
+++ final/echo.v
@@ -17,47 +17,88 @@
 `define DELAY3 15'd14400 
 `define DELAY4 15'd19200
 `define DELAY5 15'd24000
+`define MAX_ADDR 15'd23999
+`define RAM_WIDTH 16
+`define RAM_DEPTH 15 //RAM is capable of storing 2^15 addresses
 
 module echo(
 	input clk,
 	input reset,
-	input [15:0] sample_in,
-	input ready,
+	input signed [15:0] sample_in,
+	input in_ready,
 	input next_D,
 	input next_H,
-	output [15:0] echoed,
-	output flopped_ready
+	output signed [15:0] out,
+	output out_ready
 );
+
+assign out_ready = in_ready;
+
 
 wire [2:0] next_delay_state, curr_delay_state;
 wire [4:0] delay;
-dffre #(3) delay_ff(.clk(clk), .r(reset), .en(next_D), .d(next_delay_state), .q(curr_delay_state));
+wire signed [15:0] echo;
+wire [14:0] next_read_addr, curr_read_addr, next_write_addr, curr_write_addr;
 
-assign next_delay_state = (curr_delay_state == 3'd5) ? 3'd1 : curr_delay_state + 3'd1;
+dffre #(3) delay_ff(.clk(clk), .r(reset), .en(next_D), .d(next_delay_state), .q(curr_delay_state));
+dffre #(2) att_ff(.clk(clk), .r(reset), .en(next_H), .d(next_att_state), .q(curr_att_state));
+dffr #(15) write_ff(.clk(clk), .r(reset), .d(next_write_addr), .q(curr_write_addr));
+dffr #(15) read_ff(.clk(clk), .r(reset), .d(next_read_addr), .q(curr_read_addr));
+
+
+assign next_delay_state = (curr_delay_state == 3'd4) ? 3'd0 : curr_delay_state + 3'd1;
+
+assign next_att_state = curr_att_state + 1;
+
+assign next_write_addr = (curr_write_addr == MAX_ADDR) ? 15'd0 : curr_write_addr + 15'd1;
+assign next_read_addr = (curr_read_addr == MAX_ADDR) ? 15'd0 : curr_read_addr + 15'd1;
 
 always @(*) begin
-	
 	case(curr_delay_state)
-	 3'd1 : begin
+	 3'd0 : begin
 		delay = `DELAY1;
+	 end
+	 3'd1: begin
+		delay = `DELAY2;
 	 end
 	 3'd2: begin
-		delay = `DELAY1;
-	 end
-	 3'd3: begin
 		delay = `DELAY3;
 	 end
-	 3'd4: begin
+	 3'd3: begin
 		delay = `DELAY4;
 	 end
-	 3'd5: begin
+	 3'd4: begin
 		delay = `DELAY5;
 	 end
 	 default : begin
 		delay = `DELAY1;
 	 end
 	endcase
-	
 end
+
+/*
+RAM that stores delayed values. Write address should ALWAYS lead read_addr by `delay' addresses, assuming both 
+read_addr and write_addr increment by 1 at each step.
+We use port A only to write, and port B to read. We don't care about value returned from addr_A.
+*/
+RAM #(RAM_WIDTH, RAM_DEPTH) ram_1w2r  (
+    .clka(clk), //Read-write port a, only used to write
+    .wea(1), //Port a used to write to RAM
+    .addra(curr_write_addr), 
+    .dina(sample_in),
+    .douta(),
+    .clkb(clk), // Read-only port B 
+    .addrb(curr_read_addr),
+    .doutb (delayed)
+);
+
+shifter #(16) attenuator(
+	.in(delayed),
+	.distance(curr_att_state), 
+	.direction(1), //Shifts right, preserves sign 
+	.out(echo)
+);
+
+assign out = sample_in + echo;
 
 endmodule
