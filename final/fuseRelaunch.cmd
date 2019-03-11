005ae8f4edd31fde8663271936f48332fdea6002
Avoy Datta <avoy.datta@stanford.edu>
2019-03-11 13:14:41 -0700

    Deleted redundancies

--- final/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "Z:/groups/12/lab5/wave_capture_tb_isim_beh.exe" -prj "Z:/groups/12/lab5/wave_capture_tb_beh.prj" "work.wave_capture_tb" "work.glbl" 
