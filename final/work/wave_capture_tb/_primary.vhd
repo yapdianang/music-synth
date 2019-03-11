311bf6dc501e1a3d4b68e35f31f46358817a9c2c
Avoy Datta <avoy.datta@stanford.edu>
2019-03-11 13:01:01 -0700

    Restructured Repo

--- final/work/wave_capture_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity wave_capture_tb is
-end wave_capture_tb;
--- final/work/wave_display_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity wave_display_tb is
-end wave_display_tb;
